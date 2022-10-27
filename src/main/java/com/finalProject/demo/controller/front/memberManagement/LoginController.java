3c081ed85934036d1b3632a5923f18f6e46fa49c
YuLunPan <mini861021@gmail.com>
2022-10-27 15:53:54 +0800

    20221027更新

--- src/main/java/com/finalProject/demo/controller/front/memberManagement/LoginController.java
+++ src/main/java/com/finalProject/demo/controller/front/memberManagement/LoginController.java
@@ -7,8 +7,10 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -64,9 +66,55 @@ public class LoginController {
 					return "/front/index";
 				
 				} else {
-					re.addAttribute("Msg", "登入失敗!"); 			   // 畫面顯示：登入失敗!
-					return "redirect:/member/login"; 				   // 返回登入畫面
+					re.addFlashAttribute("Msg", "登入失敗!"); 			   // 畫面顯示：登入失敗!
+					return "front/member/login"; 				   // 返回登入畫面
 				}
 		}
-
+		// ---- 顯示修改會員資料 ----
+		@GetMapping("/member/user")
+		public String viewUser(HttpServletRequest request, Member member, Model model) {
+			HttpSession session = request.getSession();
+			String email = session.getAttribute("email").toString(); // 使用Session是因為要用抓這人的帳密來判斷這人的資料
+			String password = session.getAttribute("password").toString();
+			List<Member> userList = mService.viewUser(email, password);
+			if (userList.size() > 0){
+				// 表示有資料
+				model.addAttribute("email", userList.get(0).getEmail());
+				model.addAttribute("memberName", userList.get(0).getMemberName());
+				model.addAttribute("birthday", userList.get(0).getBirthday());
+				model.addAttribute("phone", userList.get(0).getPhone());
+//				System.out.println( userList.get(0).getMach());
+				return "front/member/user";
+			}else {
+				//表示帳密錯誤 或者Session有問題
+				session.removeAttribute("email"); 	// 刪掉
+				session.removeAttribute("password");
+				return "redirect:index";
+				
+			}
+		}
+		// ===================================== 送出：修改使用者資料 =======================================
+		@PostMapping("/member/usersubmit")
+		public String UpdateUser(HttpServletRequest request, 
+				@ModelAttribute("usersubmit")Member member, Model model) {
+			System.out.println("===============送出修改按鈕=================");
+			
+			HttpSession session = request.getSession();
+			String email = session.getAttribute("email").toString(); // 使用Session是因為要用抓這人的帳密來判斷這人的資料
+			String password = session.getAttribute("password").toString();
+			member.setEmail(email);
+			member.setPassword(password);
+			
+			int isUpdate = mService.updateUser(member);              // 把資料放進去(插入)
+			if(isUpdate > 0) {
+//				model = getViewUser(request, model);		// 如果要顯示更新成功這三段就要開啟getViewUser方法
+//				model.addAttribute("Msg", "*更新成功!!");
+//				return "user/user";
+				return "redirect:member/user";						// 使用此return是不會顯示更新成功
+			}
+			else {
+				model.addAttribute("Msg", "更新失敗!"); // 畫面顯示：更新失敗!
+				return "front/member/login"; 		   // 返回登入畫面
+			}
+		}
 }
